f3a5f9086753698edd3fe3bcd7710cca76fb9881
namvu1132 <69331696+namvu1132@users.noreply.github.com>
2020-08-11 15:05:12 +0700

    Update Exercise2.c

--- Exercise2.c
+++ Exercise2.c
@@ -21,9 +21,10 @@ int i,j;
 for(i=0;i<n-1;i++){
 	for(j=i+1;j<=n-1;j++){
 		if(strcmp(str[i],str[j])!=0){
-			count++
+			count++;
 		}
 	}
+}
 	printf("%d",count);
 }
 int main(int argc, char *argv[]) {
